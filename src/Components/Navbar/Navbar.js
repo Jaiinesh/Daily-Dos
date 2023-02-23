a52cb2a3b2c4b847f54d8851e1bff9c86e5d1643
Jaineh Panchal <panchaljainesh2001@gmail.com>
2023-02-23 19:52:19 +0530

    product enhencment

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div>
       <div className='Black-navbar' >
-        <h1 className='Todos-logo' >To-Dos</h1>
+        <h1 className='Todos-logo' >Daily-Dos</h1>
         <h1 className='More-from-us-btn' >More from Us!</h1>
       </div>
     </div>
